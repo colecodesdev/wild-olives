a19d78d3203e82adbc5c9bf9a443cc0fd8f29c25
colto <creill14@my.wgu.edu>
2026-03-06 21:26:38 -0600

    add smooth page transition

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,12 +1,21 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useLocation, Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
 export default function MainLayout() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }, [pathname])
+
   return (
     <>
       <Navbar />
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
       <Footer />
     </>
   )
